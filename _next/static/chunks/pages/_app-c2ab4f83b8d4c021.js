e6f2f46835ec706a2c163c389ed7f3c909a8ed8a
Denys <pjubullet816@gmail.com>
2024-04-07 19:30:05 +0900

    first build

---
+++ _next/static/chunks/pages/_app-c2ab4f83b8d4c021.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2888],{48186:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(35962)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[9774,179],function(){return _(48186),_(40694)}),_N_E=n.O()}]);
